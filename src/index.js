34919a5967953c15011ce57a548ce181a463d277
Tina <nguyennga.hcm@gmail.com>
2021-09-30 13:48:49 +0700

    add the redux

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import { Provider } from "react-redux";
-import store from "../src/redux/store";
+import store from "./redux/store";
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
